1e66b484e715b782237fec56be307de6bf5b4ffd
Matt Teran <matthew.a.teran@gmail.com>
2020-09-14 18:51:40 -0500

    Added basic component structure

---
+++ src/components/Header/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const header = () => (
+    <h1>Header</h1>
+)
+
+export default header;
---
+++ src/containers/VehicleManager/VehicleManager.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class VehicleManager extends Component{
+    render(){
+        return (
+            <h1>Vehicle Manager</h1>
+        );
+    }
+}
+
+export default VehicleManager;
