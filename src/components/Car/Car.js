6639c8a75d02a9b30692e5798d052f6f1174e943
Matt Teran <matthew.a.teran@gmail.com>
2020-09-14 19:28:06 -0500

    Added more styling to the car tiles

---
+++ src/components/Car/Car.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './Car.module.css';
+
+const car = () => {
+    return (
+        <div className={classes.Car}>
+            Car 1
+        </div>
+    )
+}
+
+export default car;
--- src/components/Cars/Cars.js
+++ src/components/Cars/Cars.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import Car from '../Car/Car';
+
 import classes from './Cars.module.css';
 
 const cars = () => (
-    <div className={classes.Cars}>Cars</div>
+    <div className={classes.Cars}>
+        <Car/>
+        <Car/>
+        <Car/>
+        <Car/>
+        <Car/>
+        <Car/>
+    </div>
 )
 
 export default cars;
